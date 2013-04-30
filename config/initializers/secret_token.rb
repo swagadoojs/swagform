9957dbf1757469a0e68676188cb298c696dac280
Jimmy Sander <jimmy@swagadoo.com>
2013-04-30 12:22:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swagform::Application.config.secret_token = 'bae2529f0cbfe4bc1b7ccc230eb3a88c5483d083746abce8c77ec56ac9fc54e6f0c4c1105c444771c6cdf9e50c3e69fb3d306b0b7d57c41216ec6d82da4fa5d0'
